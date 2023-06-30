5987ce12b26e3b35e5e0a2acb3e9aab3cf633bae
ChzenChzen <fedorovdanila@me.com>
2023-06-30 23:56:09 +0700

    refactor into builder pattern

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,66 @@
+use snafu::{prelude::*, Snafu};
 use tokio::task::JoinHandle;
-use tracing::{log::SetLoggerError, subscriber, subscriber::SetGlobalDefaultError, Subscriber};
+use tracing::{log::SetLoggerError, subscriber, subscriber::SetGlobalDefaultError};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
-use tracing_subscriber::{fmt::MakeWriter, layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
-#[derive(thiserror::Error, Debug)]
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Snafu, Debug)]
 pub enum Error {
-    #[error("Failed to setup redirection of all logs to `LogTracer`: {0}")]
-    LogTracerInitialization(#[source] SetLoggerError),
-    #[error("Failed to set provided subscriber as global default: {0}")]
-    SetGlobalDefault(#[source] SetGlobalDefaultError),
+    #[snafu(display("Failed to setup redirection of all logs to `LogTracer`"))]
+    LogTracerInitialization { source: SetLoggerError },
+    #[snafu(display("Failed to set provided subscriber as global default"))]
+    SetGlobalDefault { source: SetGlobalDefaultError },
 }
 
-/// Compose multiple layers into a `tracing`'s subscriber.
-///
-/// # Implementation Notes
-///
-/// We are using `impl Subscriber` as return type to avoid having to spell out the actual
-/// type of the returned subscriber, which is indeed quite complex.
-pub fn build_subscriber<Sink>(
-    name: &str,
-    env_filter: &str,
-    sink: Sink,
-) -> Box<dyn Subscriber + Send + Sync>
-where
-    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
-{
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name.to_owned(), sink);
-    Box::new(
-        Registry::default()
-            .with(env_filter)
-            .with(JsonStorageLayer)
-            .with(formatting_layer),
-    )
+#[derive(derive_builder::Builder)]
+#[builder(pattern = "owned")]
+pub struct Telemetrio {
+    #[builder(setter(into), default = r#"env!("CARGO_CRATE_NAME").into()"#)]
+    name: String,
+    #[builder(setter(into), default = r#""info".into()"#)]
+    env_filter: String,
+    #[builder(setter(custom), default = "Box::new(|| Box::new(std::io::stdout()))")]
+    sink: Box<dyn Fn() -> Box<dyn std::io::Write> + Send + Sync + 'static>,
 }
 
-pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) -> Result<(), Error> {
-    LogTracer::init().map_err(Error::LogTracerInitialization)?;
-    subscriber::set_global_default(subscriber).map_err(Error::SetGlobalDefault)?;
+impl Telemetrio {
+    pub fn init(self) -> Result<()> {
+        let Self {
+            name,
+            env_filter,
+            sink,
+        } = self;
+        let env_filter =
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+        let formatting_layer = BunyanFormattingLayer::new(name, sink);
+        let subscriber = Box::new(
+            Registry::default()
+                .with(env_filter)
+                .with(JsonStorageLayer)
+                .with(formatting_layer),
+        );
 
-    Ok(())
+        LogTracer::init().context(LogTracerInitializationSnafu)?;
+        subscriber::set_global_default(subscriber).context(SetGlobalDefaultSnafu)?;
+
+        Ok(())
+    }
+}
+
+impl TelemetrioBuilder {
+    pub fn sink<T, F>(self, sink: F) -> Self
+    where
+        T: std::io::Write + Send + Sync + 'static,
+        F: Fn() -> T + Send + Sync + 'static,
+    {
+        Self {
+            sink: Some(Box::new(move || Box::new(sink()))),
+            ..self
+        }
+    }
 }
 
 pub fn spawn_blocking_with_tracing<F, R>(f: F) -> JoinHandle<R>
@@ -51,4 +70,14 @@ where
 {
     let current_span = tracing::Span::current();
     tokio::task::spawn_blocking(move || current_span.in_scope(f))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn builder() {
+        let _ = TelemetrioBuilder::default().sink(std::io::stderr).build();
+    }
 }
